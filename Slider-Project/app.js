a75b77f8d7db6f79ef84b262dc08227f54e89ee6
Samridhhi Sharma <133076418+SharmaSamridhhi@users.noreply.github.com>
2024-05-09 17:32:12 +0000

    container config done

---
+++ Slider-Project/app.js
@@ -0,0 +1,7 @@
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const slider = document.querySelector('.slider')
+
+right.addEventListener('click' , ()=>{
+    console.log('right clicked')
+})
