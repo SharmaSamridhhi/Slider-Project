00dabd315139c489cf1762d522e0ea904be78ef4
Samridhhi Sharma <133076418+SharmaSamridhhi@users.noreply.github.com>
2024-05-10 11:03:02 +0000

    container config done

--- Slider-Project/app.js
+++ Slider-Project/app.js
@@ -1,7 +1,44 @@
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const slider = document.querySelector('.slider')
+const images = document.querySelectorAll('.image')
+const bottom = document.querySelector('.bottom')
+
+let slideNumber = 1;
+let length = images.length
+const nextSlide = () =>{
+    slider.style.transform = `translateX(-${slideNumber * 800 }px)`;
+    slideNumber++;
+}
+const prevSlide = () =>{
+    slider.style.transform = `translateX(-${(slideNumber - 2) * 800 }px)`;
+    slideNumber--;
+}
+const getFirstSlide = () =>{
+    slider.style.transform = `translateX(0px)`;
+    slideNumber = 1;
+}
+const getLastSlide = () =>{
+    slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
+    slideNumber = length;
+}
+
 
 right.addEventListener('click' , ()=>{
-    console.log('right clicked')
+    slideNumber < length ? nextSlide() : getFirstSlide();
+    
 })
+
+left.addEventListener('click' , ()=>{
+    slideNumber > 1 ? prevSlide() : getLastSlide();
+    
+})
+
+for (let i = 0; i < length; i++) {
+    const div = document.createElement('div');
+    div.className = 'button'
+    bottom.appendChild(div)
+}
+
+const buttons = document.querySelectorAll('.button')
+buttons[0].style.backgroundColor = 'black'
